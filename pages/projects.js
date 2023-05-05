de1a36f4253750d66f8532458a8866ca72e83344
SOL X <john.forfar@gmail.com>
2023-05-05 19:55:09 +1000

    image fixes

--- pages/projects.js
+++ pages/projects.js
@@ -1,8 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
-
-import { motion } from "framer-motion";
+import portfolio1 from "/portfolio1.jpg";
 import Link from "next/link";
 export default function Qualifcations() {
   return (
@@ -19,11 +17,11 @@ export default function Qualifcations() {
         <div className="max-w-screen-xl mx-auto px-4 flex">
 
           {/* <!-- Grid column --> */}
-          <div className="w-full p-4 sm:w-1/2 lg:w-1/3">
+          <div className="p-4 sm:w-1/2 lg:w-1/3">
             {/* <!-- Column contents --> */}
             <div className="bg-white rounded-lg shadow-lg">
               {/* <!-- Card contents --> */}
-              <Image src="/thumbnails/florian-olivo-4hbJ-eymZ1o-unsplash.jpg" alt="" className="rounded-t-lg" />
+              <Image src={portfolio1} className="rounded-t-lg" width={20} height={20} />
               <div className="p-2">
                 <h3 className="font-bold mb-2 text-2xl text-purple-700">Defi.Compare</h3>
                 <p className="text-purple-500 mb-2">Blockchain fee comparison dashboard using Python, TypeScript, React, Hasura & Timescale.</p>
@@ -37,7 +35,7 @@ export default function Qualifcations() {
             {/* <!-- Column contents --> */}
             <div className="bg-white rounded-lg shadow-lg">
               {/* <!-- Card contents --> */}
-              <Image src="/thumbnails/florian-olivo-4hbJ-eymZ1o-unsplash.jpg" alt="" className="rounded-t-lg" />
+              <Image src={portfolio1} alt="" className="rounded-t-lg" width={20} height={20} />
               <div className="p-2">
                 <h3 className="font-bold mb-2 text-2xl text-purple-700">JohnForfar.com</h3>
                 <p className="text-purple-500 mb-2">Porfolio website using JavaScript, React, Next, SendGrid, Tailwind, Twilio & MongoDB.</p>
@@ -51,7 +49,7 @@ export default function Qualifcations() {
             {/* <!-- Column contents --> */}
             <div className="bg-white rounded-lg shadow-lg">
               {/* <!-- Card contents --> */}
-              <Image src="/thumbnails/florian-olivo-4hbJ-eymZ1o-unsplash.jpg" alt="" className="rounded-t-lg" />
+              <Image src="{portfolio1}" alt="" className="rounded-t-lg" width={20} height={20} />
               <div className="p-2">
                 <h3 className="font-bold mb-2 text-2xl text-purple-700">HeyToly.com</h3>
                 <p className="text-purple-500 mb-2">Solana ecosystem discovery AI chat bot using Scrapers, ChatGPT Davinci API, React & Next.</p>
