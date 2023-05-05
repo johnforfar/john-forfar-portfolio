2aada20638a2ae798b4c2a610804c1b650d72b61
SOL X <john.forfar@gmail.com>
2023-05-06 09:41:38 +1000

    Footer fix

--- pages/components/Ticker.js
+++ pages/components/Ticker.js
@@ -3,6 +3,6 @@ import React from "react";
 export default function Ticker() {
   return <div className="text-sm font-thin flex-wrap p-2">
       This site was built using
-      ReactJS, NextJS, SendGrid, Tailwind, Twilio & MongoDB
+      ReactJS, NextJS, Tailwind, MongoDB, SendGrid & Twilio
     </div>;
 }
